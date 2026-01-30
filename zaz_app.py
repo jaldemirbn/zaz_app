9f7148b18d632094c7e4e1dd4fc61a9054e313c3
jaldemirbn <jaldemirbn@gmail.com>
2026-01-30 19:11:55 -0300

    Update zaz_app.py

--- zaz_app.py
+++ zaz_app.py
@@ -1,7 +1,36 @@
 import streamlit as st
+from supabase import create_client
 from modules.ui_ideias import render_etapa_ideias
 
 
+# ======================
+# SUPABASE
+# ======================
+@st.cache_resource
+def conectar():
+    return create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
+
+
+def validar_usuario(email, senha):
+    supabase = conectar()
+
+    resp = (
+        supabase.table("usuarios")
+        .select("*")
+        .eq("email", email)
+        .eq("senha", senha)
+        .execute()
+    )
+
+    return len(resp.data) > 0
+
+
+# ======================
+# SESSION
+# ======================
 if "logado" not in st.session_state:
     st.session_state.logado = False
 
@@ -11,10 +40,12 @@ if "logado" not in st.session_state:
 # ======================
 if not st.session_state.logado:
 
+    st.subheader("Entrar")
+
     email = st.text_input("Email")
     senha = st.text_input("Senha", type="password")
 
-    if st.button("Entrar"):
+    if st.button("Entrar", use_container_width=True):
 
         if validar_usuario(email, senha):
             st.session_state.logado = True
@@ -24,8 +55,7 @@ if not st.session_state.logado:
 
 
 # ======================
-# APP (DEPOIS DO LOGIN)
+# APP
 # ======================
 else:
-
     render_etapa_ideias()
