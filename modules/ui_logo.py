b903fe4ec871350e2af80e265e9094fff03e3ea7
jaldemirbn <jaldemirbn@gmail.com>
2026-01-29 21:22:23 -0300

    Update ui_logo.py

--- modules/ui_logo.py
+++ modules/ui_logo.py
@@ -1,1 +1,19 @@
+import streamlit as st
+
+
+# =========================================
+# MÓDULO BRANDING — LOGO
+# Responsabilidade única:
+# renderizar a logomarca centralizada
+# =========================================
+
+
+def render_logo():
+
+    col1, col2, col3 = st.columns([1, 1, 1])
+
+    with col2:
+        st.image("assets/logo.png", width=450)
+
+    st.divider()
 
