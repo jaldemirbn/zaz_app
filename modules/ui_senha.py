0d847718ffba570b95c4cc335e2c4da7ec292efc
jaldemirbn <jaldemirbn@gmail.com>
2026-01-31 12:27:33 -0300

    Update ui_senha.py

--- modules/ui_senha.py
+++ modules/ui_senha.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+
+def render_trocar_senha(atualizar_senha):
+
+    email = st.text_input("Email", key="alt_email")
+    senha = st.text_input("Nova senha", type="password", key="alt_senha")
+
+    if st.button("Atualizar senha", use_container_width=True):
+        atualizar_senha(email, senha)
+        st.success("Senha atualizada com sucesso.")
 
