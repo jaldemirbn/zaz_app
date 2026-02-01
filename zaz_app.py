cb76c021e5656c6f13fbc307d89a5ca688fb7800
jaldemirbn <jaldemirbn@gmail.com>
2026-01-31 23:28:02 -0300

    Update zaz_app.py

--- zaz_app.py
+++ zaz_app.py
@@ -105,7 +105,7 @@ def criar_usuario(email, senha):
 
     supabase.table("usuarios").insert(dados).execute()
 
-    link = f"https://SEU_APP.streamlit.app/?token={token}"
+    link = f"https://zazapp.streamlit.app/?token={token}"
 
     enviar_email_confirmacao(email, link)
 
