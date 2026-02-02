77fa774133c8acbe92d44171c455f84ac65cc449
jaldemirbn <jaldemirbn@gmail.com>
2026-02-02 20:34:37 -0300

    Update zaz_app.py

--- zaz_app.py
+++ zaz_app.py
@@ -12,7 +12,7 @@ from modules.ui_imagens import render_etapa_imagens
 from modules.ui_post import render_etapa_post
 from modules.ui_canvas import render_etapa_canvas
 from modules.ui_legenda import render_etapa_legenda
-
+from modules.ui_postagem import render_etapa_postagem
 
 # =====================================================
 # CONFIG
@@ -108,4 +108,4 @@ render_etapa_imagens()
 render_etapa_post()
 render_etapa_canvas()
 render_etapa_legenda()
-
+render_etapa_postagem()
