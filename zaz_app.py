8e4890695b5dbead3781f3750210bc212b3c40bf
jaldemirbn <jaldemirbn@gmail.com>
2026-02-02 17:03:25 -0300

    Update zaz_app.py

--- zaz_app.py
+++ zaz_app.py
@@ -9,6 +9,7 @@ from modules.ui_ideias import render_etapa_ideias
 from modules.ui_headline import render_etapa_headline
 from modules.ui_conceito import render_etapa_conceito
 from modules.ui_imagens import render_etapa_imagens
+from modules.ui_post import render_etapa_post
 from modules.ui_postagem import render_etapa_postagem
 from modules.ui_historico import render_etapa_historico
 
@@ -104,5 +105,6 @@ render_etapa_ideias()
 render_etapa_headline()
 render_etapa_conceito()
 render_etapa_imagens()
+render_etapa_post()
 render_etapa_postagem()
 render_etapa_historico()
