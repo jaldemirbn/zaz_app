724989f2eab7a50fc5b4e8456ed761991ce097d5
jaldemirbn <jaldemirbn@gmail.com>
2026-02-03 23:50:15 -0300

    Update ui_conceito.py

--- modules/ui_conceito.py
+++ modules/ui_conceito.py
@@ -209,6 +209,21 @@ def render_etapa_conceito():
         )
 
 
+    # BOTÃO — VOLTAR
+    if st.button(
+        "⬅ Voltar",
+        key="btn_voltar",
+        use_container_width=True
+    ):
+
+        limpar_conceito()
+        limpar_imagens()
+        limpar_texto()
+        limpar_postagem()
+
+        st.session_state.etapa_4_liberada = False
+        st.session_state.etapa = 2
+        st.rerun()
 
     # -------------------------------------------------
     # BOTÃO — VOLTAR
@@ -225,4 +240,5 @@ def render_etapa_conceito():
         st.session_state.etapa_4_liberada = False
         st.session_state.etapa = 2
         st.rerun()
+
 
